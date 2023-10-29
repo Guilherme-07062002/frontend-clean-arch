336384161518a5017d046e0cc8e57452dafb4fc6
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-29 17:34:06 -0300

    feat: adjust view layer

--- src/application/actions/count.ts
+++ src/application/actions/count.ts
@@ -2,5 +2,5 @@ import { ActionTypes } from "./types";
 
 export interface CountAction {
   type: ActionTypes.COUNT;
-  info: 'add +1 to counter'
+  info: 'Add +1 to counter'
 };
--- src/views/components/App.tsx
+++ src/views/components/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { AppState } from "../../application/store";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state: AppState) => state.count);
+  const dispatch = useDispatch();
+
+  function setCount() {
+    dispatch({ type: "COUNT" });
+  }
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount()}>count is {count}</button>
       </div>
     </>
   );
